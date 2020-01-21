526e9085c8f78afc7eabaeb85e545a640475c4db
paulnarhi <paulnarhi@gmail.com>
2020-01-21 21:15:27 +0200

    Added parsing for real estate rent price for a single apartment

--- Real-estate-price-bot/testi.py
+++ Real-estate-price-bot/testi.py
@@ -1,3 +1,19 @@
-import sys
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen as uReq
 
-print(sys.version)
+page_url = "https://www.vuokraovi.com/vuokra-asunnot/Turku"
+
+# open and close connection and save html to variable
+uClient = uReq(page_url)
+page_html = uClient.read()
+uClient.close()
+
+# html parsing
+page_soup = soup(page_html, "html.parser")
+
+# grab individual real estate
+containers = page_soup.findAll("div", {"class": "list-item-container"})
+
+# get price of real estate
+print(containers[0].div.a.div.find_next_sibling().ul.find("li", {"class": "rent"}).span.text.strip())
+
